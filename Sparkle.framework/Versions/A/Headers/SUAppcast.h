530d6bc4d3295f362cea40390a2a4ac03f0e15be
Oliver Raduner <github@raduner.ch>
2014-01-16 11:42:34 +0100

    Updated Sparkle.framework to Version 1.5b6

--- Sparkle.framework/Versions/A/Headers/SUAppcast.h
+++ Sparkle.framework/Versions/A/Headers/SUAppcast.h
@@ -6,22 +6,31 @@
 //  Copyright 2006 Andy Matuschak. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#ifndef SUAPPCAST_H
+#define SUAPPCAST_H
 
-@class RSS, SUAppcastItem;
-@interface SUAppcast : NSObject {
+@class SUAppcastItem;
+@interface SUAppcast : NSObject
+{
+@private
 	NSArray *items;
+	NSString *userAgentString;
 	id delegate;
+	NSString *downloadFilename;
+	NSURLDownload *download;
 }
 
 - (void)fetchAppcastFromURL:(NSURL *)url;
 - (void)setDelegate:delegate;
+- (void)setUserAgentString:(NSString *)userAgentString;
 
-- (SUAppcastItem *)newestItem;
 - (NSArray *)items;
 
 @end
 
 @interface NSObject (SUAppcastDelegate)
-- appcastDidFinishLoading:(SUAppcast *)appcast;
+- (void)appcastDidFinishLoading:(SUAppcast *)appcast;
+- (void)appcast:(SUAppcast *)appcast failedToLoadWithError:(NSError *)error;
 @end
+
+#endif
--- Sparkle.framework/Versions/A/Headers/SUAppcastItem.h
+++ Sparkle.framework/Versions/A/Headers/SUAppcastItem.h
@@ -6,52 +6,55 @@
 //  Copyright 2006 Andy Matuschak. All rights reserved.
 //
 
-#import <Cocoa/Cocoa.h>
+#ifndef SUAPPCASTITEM_H
+#define SUAPPCASTITEM_H
 
-
-@interface SUAppcastItem : NSObject {
+@interface SUAppcastItem : NSObject
+{
+@private
 	NSString *title;
 	NSDate *date;
-	NSString *description;
+	NSString *itemDescription;
 	
 	NSURL *releaseNotesURL;
 	
-	NSString *DSASignature;
-	NSString *MD5Sum;
+	NSString *DSASignature;	
+	NSString *minimumSystemVersion;
+    NSString *maximumSystemVersion;
 	
 	NSURL *fileURL;
-	NSString *fileVersion;
 	NSString *versionString;
+	NSString *displayVersionString;
+
+	NSDictionary *deltaUpdates;
+
+	NSDictionary *propertiesDictionary;
+	
+	NSURL *infoURL;	// UK 2007-08-31
 }
 
 // Initializes with data from a dictionary provided by the RSS class.
 - initWithDictionary:(NSDictionary *)dict;
+- initWithDictionary:(NSDictionary *)dict failureReason:(NSString**)error;
 
 - (NSString *)title;
-- (void)setTitle:(NSString *)aTitle;
-
-- (NSDate *)date;
-- (void)setDate:(NSDate *)aDate;
-
-- (NSString *)description;
-- (void)setDescription:(NSString *)aDescription;
-
-- (NSURL *)releaseNotesURL;
-- (void)setReleaseNotesURL:(NSURL *)aReleaseNotesURL;
-
-- (NSString *)DSASignature;
-- (void)setDSASignature:(NSString *)aDSASignature;
-
-- (NSString *)MD5Sum;
-- (void)setMD5Sum:(NSString *)aMd5Sum;
-
-- (NSURL *)fileURL;
-- (void)setFileURL:(NSURL *)aFileURL;
-
-- (NSString *)fileVersion;
-- (void)setFileVersion:(NSString *)aFileVersion;
-
 - (NSString *)versionString;
-- (void)setVersionString:(NSString *)versionString;
+- (NSString *)displayVersionString;
+- (NSDate *)date;
+- (NSString *)itemDescription;
+- (NSURL *)releaseNotesURL;
+- (NSURL *)fileURL;
+- (NSString *)DSASignature;
+- (NSString *)minimumSystemVersion;
+- (NSString *)maximumSystemVersion;
+- (NSDictionary *)deltaUpdates;
+- (BOOL)isDeltaUpdate;
+
+// Returns the dictionary provided in initWithDictionary; this might be useful later for extensions.
+- (NSDictionary *)propertiesDictionary;
+
+- (NSURL *)infoURL;						// UK 2007-08-31
 
 @end
+
+#endif
